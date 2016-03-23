046c83d3bdc76811557a96a89b51d6c494eb2a4b
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-23 17:16:00 -0500

    v0.3.2

--- evidencemodeler.rb
+++ evidencemodeler.rb
@@ -1,9 +1,9 @@
 class Evidencemodeler < Formula
   desc "EVidenceModeler combines gene prediction/evidence into weighted gene predictions"
   homepage "http://evidencemodeler.github.io"
-  url "https://github.com/EVidenceModeler/EVidenceModeler/archive/v1.1.1.tar.gz"
-  version "1.1.1"
-  sha256 "9ef89095fe37326d44eb95cc5b7dacae301aafa1dedfc13c17fcaec1c9388757"
+  url "https://github.com/nextgenusfs/EVidenceModeler/archive/1.1.2.tar.gz"
+  version "1.1.2"
+  sha256 ""
   # tag "bioinformatics"
   
   def install
