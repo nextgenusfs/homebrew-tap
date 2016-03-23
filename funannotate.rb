6bfe4bf39f128c9bf99c8be489cf933a8c6051ce
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-23 10:46:58 -0500

    update

--- funannotate.rb
+++ funannotate.rb
@@ -102,11 +102,11 @@ class Funannotate < Formula
         system "python", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
-    system "python", *Language::Python.setup_install_args(libexec)
+    #ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python2.7/site-packages"
+    #system "python", *Language::Python.setup_install_args(libexec)
     
-    bin.install Dir[libexec/"bin/*"]
-    bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
+    #bin.install Dir[libexec/"bin/*"]
+    #bin.env_script_all_files(libexec/"bin", :PYTHONPATH => ENV["PYTHONPATH"])
     
     libexec.install Dir["*"]
     bin.install_symlink libexec/"funannotate"
