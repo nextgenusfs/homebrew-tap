a48c49f0273699843db291d0d0b432e44c3b8fee
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-23 16:01:30 -0500

    v0.3.2

---
+++ ufits.rb
@@ -0,0 +1,30 @@
+class Ufits < Formula
+  desc "USEARCH Fungal ITS Clustering scripts"
+  homepage "https://github.com/nextgenusfs/ufits"
+  url "https://github.com/nextgenusfs/ufits/archive/0.3.2.tar.gz"
+  version "0.3.2"
+  sha256 "0e4ee60dae210516855d15ac29538ed1fe15bde16ad3dec7a9a7aa7b9b777cad"
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"ufits"
+  end
+
+  def caveats; <<-EOS.undent
+    Additional Dependencies:
+        UFITS requires USEARCH8, get here: http://www.drive5.com/usearch/download.html
+        
+      Make executable:
+         sudo chmod +x /path/usearch/binary
+           
+      Add to $PATH (UFITS uses usearch8 as default name):
+         sudo ln -s /path/usearch/binary /usr/local/bin/usearch8  
+        
+    EOS
+  end
+
+
+  test do
+    system "#{bin}/ufits version"
+  end
+end
