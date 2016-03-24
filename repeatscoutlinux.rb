b3aca2fa7f7b69f5a6665282b5b0019eca1d666d
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-24 10:03:20 -0500

    updates

--- repeatscoutlinux.rb
+++ repeatscoutlinux.rb
@@ -1,4 +1,4 @@
-class Repeatscout-linux < Formula
+class Repeatscoutlinux < Formula
   homepage "http://bix.ucsd.edu/repeatscout/"
   # doi "10.1093/bioinformatics/bti1018"
   # tag "bioinformatics"
