2153e089cc5331dee610949e536db69661c9e55d
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-24 12:42:01 -0500

    update to v0.1.2

--- funannotate.rb
+++ funannotate.rb
@@ -24,12 +24,12 @@ class Funannotate < Formula
   depends_on "raxml" => :recommended
   depends_on "trimal" => :recommended
   depends_on "mafft" => :recommended
-  depends_on "cpanm" => :optional
+  depends_on "cpanminus" => :optional
   depends_on 'braker' => :recommended
   depends_on 'evidencemodeler' => :recommended
   depends_on 'gag' => :recommended
   # No formula: depends_on 'genemark-es' => :recommended
-  
+
   depends_on "Bio::Perl" => :perl
   depends_on "Getopt::Long" => :perl
   depends_on "Pod::Usage" => :perl
@@ -44,18 +44,18 @@ class Funannotate < Formula
   depends_on "Logger::Simple" => :perl
   depends_on "Parallel::ForkManager" => :perl
   depends_on "Scalar::Util::Numeric" => :perl
-  
+
   depends_on :python if MacOS.version <= :snow_leopard
 
-  #depends_on "biopython" => :python
-  #depends_on "natsort" => :python  
-  #depends_on "psutil" => :python  
-  #depends_on "goatools" => :python
-  #depends_on "numpy" => :python
-  #depends_on "pandas" => :python
-  #depends_on "matplotlib" => :python  
-  #depends_on "seaborn" => :python
-  #depends_on "scikit-learn" => :python
+  # No formula: depends_on "biopython" => :python
+  # No formula: depends_on "natsort" => :python  
+  # No formula: depends_on "psutil" => :python  
+  # No formula: depends_on "goatools" => :python
+  # No formula: depends_on "numpy" => :python
+  # No formula: depends_on "pandas" => :python
+  # No formula: depends_on "matplotlib" => :python  
+  # No formula: depends_on "seaborn" => :python
+  # No formula: depends_on "scikit-learn" => :python
 
   def install
     libexec.install Dir["*"]
@@ -66,33 +66,33 @@ class Funannotate < Formula
     Additional components of Funannotate:
       Troubleshooting, see detailed instructions:
         https://github.com/nextgenusfs/funannotate
-      
+
       Download/install GeneMark-ES/ET: (gmes_petap.pl must be in PATH)
         http://exon.gatech.edu/GeneMark/license_download.cgi
-       
+
       Install python modules via PIP:
         pip install -U biopython natsort psutil goatools numpy pandas matplotlib seaborn scikit-learn
-    
+
       Be sure to install RepeatMasker Libraries if you have not done so already.
         wget --user name --password pass http://www.girinst.org/server/RepBase/protected/repeatmaskerlibraries/repeatmaskerlibraries-20150807.tar.gz
         tar zxvf repeatmaskerlibraries-20150807.tar.gz -C #{HOMEBREW_PREFIX}/Cellar/repeatmasker/4.0.5/libexec
 
         cd #{HOMEBREW_PREFIX}/Cellar/repeatmasker/4.0.5/libexec
         ./configure <config.txt
-        
+
       Soft-link a repeatmasker utility script into the PATH:
         ln -s #{HOMEBREW_PREFIX}/Cellar/repeatmasker/4.0.5/libexec/util/rmOutToGFF3.pl #{HOMEBREW_PREFIX}/bin/rmOutToGFF3.pl
-      
+
       Export required ENV variables (your paths might differ slightly):
         export EVM_HOME=#{HOMEBREW_PREFIX}/Cellar/evidencemodeler/1.1.2
         export AUGUSTUS_CONFIG_PATH=#{HOMEBREW_PREFIX}/opt/augustus/libexec/config
         export BAMTOOLS_PATH=#{HOMEBREW_PREFIX}/Cellar/bamtools/2.4.0/bin
         export GENEMARK_PATH=/path/to/gmes_petap.pl
-          
+  
       Install Databases for funannotate:
-        cd #{prefix}/libexec
+        cd #{libexec}
         ./setup.sh
-        
+
     EOS
   end
 
@@ -100,4 +100,3 @@ class Funannotate < Formula
     system "#{bin}/funannotate version"
   end
 end
-
