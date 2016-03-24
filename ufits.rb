086ad39134c0562b01194d880fd36a96a03b32ac
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-03-24 12:48:13 -0500

    audit updates

--- ufits.rb
+++ ufits.rb
@@ -2,7 +2,6 @@ class Ufits < Formula
   desc "USEARCH Fungal ITS Clustering scripts"
   homepage "https://github.com/nextgenusfs/ufits"
   url "https://github.com/nextgenusfs/ufits/archive/0.3.2.tar.gz"
-  version "0.3.2"
   sha256 "0e4ee60dae210516855d15ac29538ed1fe15bde16ad3dec7a9a7aa7b9b777cad"
 
   def install
@@ -13,18 +12,17 @@ class Ufits < Formula
   def caveats; <<-EOS.undent
     Additional Dependencies:
         UFITS requires USEARCH8, get here: http://www.drive5.com/usearch/download.html
-        
+
       Make executable:
          sudo chmod +x /path/usearch/binary
-           
-      Add to $PATH (UFITS uses usearch8 as default name):
-         sudo ln -s /path/usearch/binary /usr/local/bin/usearch8  
   
+      Add to $PATH (UFITS uses usearch8 as default name):
+         sudo ln -s /path/usearch/binary /usr/local/bin/usearch8
+
       Python Dependencies:
-        pip install -U biopython natsort numpy pandas matplotlib      
+        pip install -U biopython natsort numpy pandas matplotlib  
     EOS
   end
-
 
   test do
     system "#{bin}/ufits version"
