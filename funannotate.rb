41aef74aff15f575688ceabbfa55eadcf912e525
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-04-11 09:01:52 -0500

    update to v0.1.6

--- funannotate.rb
+++ funannotate.rb
@@ -2,7 +2,7 @@ class Funannotate < Formula
   desc "Scripts to functionally annotate fungal genomes"
   homepage "https://github.com/nextgenusfs/funannotate"
   url "https://github.com/nextgenusfs/funannotate/archive/0.1.6.tar.gz"
-  sha256 ""
+  sha256 "c235a562e297ea9eb64ba9535d5962014b819241ebe8379beb9c3aba9e4b7f0a"
   # tag "bioinformatics"
 
   depends_on "gmap-gsnap" => :recommended
