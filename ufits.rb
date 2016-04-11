40266865c706761b3e89757d86d549fb8fdb538e
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-04-11 08:26:19 -0500

    update to v0.3.4

--- ufits.rb
+++ ufits.rb
@@ -1,11 +1,11 @@
 class Ufits < Formula
   desc "USEARCH Fungal ITS Clustering scripts"
   homepage "https://github.com/nextgenusfs/ufits"
-  url "https://github.com/nextgenusfs/ufits/archive/0.3.3.tar.gz"
-  sha256 "52325cd36cfa077432479a1f386f08878361952c989ba2e28c13d38cbd5952fd"
+  url "https://github.com/nextgenusfs/ufits/archive/0.3.4.tar.gz"
+  sha256 ""
 
   depends_on "bedtools" => :recommended
-  depends_on "vsearch" => :optional
+  depends_on "vsearch" => :recommended
 
   def install
     libexec.install Dir["*"]
@@ -20,7 +20,7 @@ class Ufits < Formula
          sudo chmod +x /path/usearch/binary
 
       Add to $PATH (UFITS uses usearch8 as default name):
-         sudo ln -s /path/usearch/binary /usr/local/bin/usearch8
+         sudo ln -s /path/usearch/binary #{HOMEBREW_PREFIX}/opt/usearch8
 
       Python Dependencies:
         pip install -U biopython natsort numpy pandas matplotlib
