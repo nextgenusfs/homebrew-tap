f6c21e5f95f890f3cdad90d689c6fd6dc468f344
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-04-19 12:54:48 -0500

    update to v0.3.7

--- ufits.rb
+++ ufits.rb
@@ -1,7 +1,7 @@
 class Ufits < Formula
   desc "USEARCH Fungal ITS Clustering scripts"
   homepage "https://github.com/nextgenusfs/ufits"
-  url "https://github.com/nextgenusfs/ufits/archive/0.3.6.tar.gz"
+  url "https://github.com/nextgenusfs/ufits/archive/0.3.7.tar.gz"
   sha256 "d102a7f6f67bfe8c951cda974577ff6743f543e63f259f0aeaa7055a42154b52"
 
   depends_on "bedtools" => :recommended
