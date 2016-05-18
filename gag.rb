f18451a6fedc76e69c38315b5ab0531b2d00bfdd
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-05-18 08:58:13 -0500

    update to v0.1.3

--- gag.rb
+++ gag.rb
@@ -1,8 +1,8 @@
 class Gag < Formula
   desc "Converts GFF into NCBI tbl format"
   homepage "https://genomeannotation.github.io/GAG/"
-  url "https://github.com/nextgenusfs/GAG/archive/0.1.2.tar.gz"
-  sha256 "6caa54f04a0f43346082f8636a8c899d58555e459a2425cc8a22c98f6efb4706"
+  url "https://github.com/nextgenusfs/GAG/archive/0.1.3.tar.gz"
+  sha256 "a009c4954f064000840dbc18b5d393ee66ce176c1bd796630bd5cb7555796fe0"
 
   def install
     libexec.install Dir["*"]
