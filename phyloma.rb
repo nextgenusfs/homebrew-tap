86448482a12c7a3a287eb8bf4e74e7062423b821
Jon Palmer <jon@Jons-MacBook-Pro.local>
2016-10-24 13:43:04 -0500

    update to v0.0.8

--- phyloma.rb
+++ phyloma.rb
@@ -1,8 +1,8 @@
 class Phyloma < Formula
   desc "Phyloma: Phylogenetic Marker-gene Analysis"
   homepage "https://github.com/nextgenusfs/phyloma"
-  url "https://github.com/nextgenusfs/phyloma/archive/0.0.7.tar.gz"
-  sha256 "081e6972f37c5b478284a3c53ea5d5b4a51f01a9c03c234d007bb7c717f834f0"
+  url "https://github.com/nextgenusfs/phyloma/archive/0.0.8.tar.gz"
+  sha256 "5c0720874a99b26f0ba7cfb2490c45431eb7306e7dc13eebf70f6f611c03aaf4"
 
   depends_on "bedtools" => :recommended
   depends_on "freebayes" => :recommended
