56d3469b30545a32feb0411cf8f1e142ec31ca16
Jon Palmer <jon@Jons-MacBook-Pro.local>
2017-01-11 11:26:05 -0600

    update to v0.8.0

---
+++ amptk.rb
@@ -0,0 +1,36 @@
+class Amptk < Formula
+  desc "AMPlicon ToolKit for NGS data"
+  homepage "https://github.com/nextgenusfs/amptk"
+  url "https://github.com/nextgenusfs/amptk/archive/0.8.0.tar.gz"
+  sha256 "155807fe7f203bdc0e299af1eb475f06f806f0e0848bdf4236dd579b5316978a"
+
+  depends_on "bedtools" => :recommended
+  depends_on "vsearch" => :recommended
+  depends_on "R" => :recommended
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink libexec/"amptk"
+  end
+
+  def caveats; <<-EOS.undent
+    Additional Dependencies:
+        AMPtk requires USEARCH9, get here: http://www.drive5.com/usearch/download.html
+
+      Make executable:
+         sudo chmod +x /path/usearch/binary
+
+      Add to $PATH (UFITS uses usearch9 as default name):
+         sudo ln -s /path/usearch/binary #{HOMEBREW_PREFIX}/opt/usearch9
+
+      Python Dependencies:
+        pip install -U biopython natsort numpy pandas matplotlib psutil h5py biom-format
+      or
+        conda install biopython natsort numpy pandas matplotlib psutil h5py biom-format
+    EOS
+  end
+
+  test do
+    system "#{bin}/amptk version"
+  end
+end
