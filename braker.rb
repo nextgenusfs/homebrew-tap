658327d7c6301f4f70eca0609d290531cd664a9b
Jon Palmer <jon@Jons-MacBook-Pro.local>
2017-07-10 15:07:28 -0500

    update to v1.11

--- braker.rb
+++ braker.rb
@@ -1,9 +1,9 @@
 class Braker < Formula
   desc "Automated training of AUGUSTUS and GeneMark"
   homepage "http://exon.gatech.edu/genemark/braker1.html"
-  url "http://bioinf.uni-greifswald.de/augustus/binaries/BRAKER1.tar.gz"
-  version "1.10"
-  sha256 "9a941992535e8fe0c324ad9c4cc58fbf76e36400a5541d45ec7236618487cd04"
+  url "http://bioinf.uni-greifswald.de/augustus/binaries/BRAKER1_v1.11.tar.gz"
+  version "1.11"
+  sha256 "cb2d9abe1720ed58753d362eee4af3791007efc617754804882d31f9fe2eab00"
 
   def install
     inreplace "braker.pl", "/usr/bin/perl", "/usr/bin/env perl"
