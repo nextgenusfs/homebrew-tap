8228df13c11c31de06dee8cc833372fff27311e6
Jon Palmer <jon@Jons-MacBook-Pro.local>
2018-01-20 15:29:08 -0600

    update to v2.0.2

--- braker.rb
+++ braker.rb
@@ -2,8 +2,8 @@ class Braker < Formula
   desc "Automated training of AUGUSTUS and GeneMark"
   homepage "http://exon.gatech.edu/genemark/braker1.html"
   url "http://bioinf.uni-greifswald.de/augustus/binaries/BRAKER2.tar.gz"
-  version "2.0.1"
-  sha256 "bb6c1a832e0228ad8e48db9d17591fab8b03962a46d55dd0693f461104f6681e"
+  version "2.0.2"
+  sha256 "e2097a9cfa9c44fe09cfc1d9c1e57605a29480a91c564aa932e0555bd57c78ab"
 
   def install
     libexec.install Dir["*"]
